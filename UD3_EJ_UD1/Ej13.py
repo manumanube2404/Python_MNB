61c7b41419e0d394964daeffc2d314edd905854f
“Manuel <manumanube2404@gmail.com>
2025-10-30 20:41:02 +0100

    EJ 13

---
+++ UD3_EJ_UD1/Ej13.py
@@ -0,0 +1,9 @@
+print("introduzca 2 numeros:")
+num1=int(input())
+num2=int(input())
+if num1>num2:
+    print(num1,num2)
+elif num2>num1:
+    print(num2,num1)
+else:
+    print("Los numeros son iguales")
