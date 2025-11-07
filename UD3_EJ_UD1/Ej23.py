13b583842a584454a19d0de90fa4f8e69a48b86c
“Manuel <manumanube2404@gmail.com>
2025-11-07 09:45:44 +0100

    Ej 23

---
+++ UD3_EJ_UD1/Ej23.py
@@ -0,0 +1,16 @@
+print("introduce un numero:")
+Neg=0
+Pos=0
+
+for i in range(1, 5):
+    numero = int(input())
+    if numero == 0:
+        break
+    
+    elif numero < 0:
+        Neg += 1
+    else:
+        Pos += 1
+
+print("Numeros negativos:", Neg)
+print("Numeros positivos:", Pos)
