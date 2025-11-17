5693dbdaa0a667dc1247f5e503070053a98265e4
“Manuel <manumanube2404@gmail.com>
2025-11-17 10:46:58 +0100

    Ej 14-15 propios

--- UD3_EJ_UD3_P2/Ej15.py
+++ UD3_EJ_UD3_P2/Ej15.py
@@ -1,3 +1,4 @@
+# Forma 1
 # print("Introduce un numero")
 # altura=int(input())
 # for i in range(1,altura+1):
@@ -7,7 +8,17 @@
 #         print("*",end="")
 #     print("")
 
-print("Introduce un numero")
+#Forma 2
+# print("Introduce un numero")
+# altura=int(input())
+# for i in range(1,altura+1):
+#     print(" "*(i-1),"*"*((2*altura+1)-(i*2)))
+
+print("dame la altura de la piramide")
 altura=int(input())
-for i in range(1,altura+1):
-    print(" "*(i-1),"*"*((2*altura+1)-(i*2)))
+for i in range(0,altura):
+    asterisco="*"*(((altura-i)*2)-1)
+    
+    espacios=" "*i
+    
+    print(espacios+asterisco)
