3e55f62b28246ce69eddb6a9dac300622fe0fd1d
“Manuel <manumanube2404@gmail.com>
2025-11-19 14:00:57 +0100

    triangulo inverso

---
+++ UD3_EJ_UD3_P2/Ej1_foro.py
@@ -0,0 +1,27 @@
+print("Introduce un numero:")
+numero=int(input())
+asterisco="*"
+espacios=" "
+print(espacios*(numero+1)+asterisco)
+for i in range (0,numero):
+    print(espacios*(numero-i)+asterisco+espacios*(i)+asterisco)
+
+
+for j in range (numero,0,-1):
+    print(espacios*(numero-j)+asterisco+espacios*(j)+asterisco)
+
+    
+print(espacios*(numero+1)+asterisco)
+    
+#
+#      *
+#     **
+#    * * 
+#   *  *
+#  *   *
+# *    *
+#
+#
+#
+#
+#
