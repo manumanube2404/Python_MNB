50ee0bf279041632151c6f2b42cfbb219b381c87
“Manuel <manumanube2404@gmail.com>
2025-11-20 12:50:40 +0100

    cuadrados-V2

--- UD3_EJ_UD3_P2/Ej03_foro.py
+++ UD3_EJ_UD3_P2/Ej03_foro.py
@@ -1,25 +1,25 @@
 
 print("introduce un numero:")
 num=int(input())
-espacio=" "
-asterisco="*"
+espacio="   "
+asterisco="***"
 num2=round(num/2)
 
 for k in range(num2):
     for i in range(3): #asteriscos x
         
         for j in range(num2):
-            print(espacio*3,end="")
+            print(espacio,end="")
             
-            if num%2==0 or j!=num2-1 and num%2!=0 or num==5:    
-                print(asterisco*3,end="")
+            if num%2==0 or j!=num2-1 and num%2!=0:    
+                print(asterisco,end="")
         print()
         
     if num%2==0 or k!=num2-1 and num%2!=0:
         for s in range(3): #asteriscos Y
                 for h in range(num2):
                     
-                    print(asterisco*3,end="")
+                    print(asterisco,end="")
                     
-                    print(espacio*3,end="")
+                    print(espacio,end="")
                 print()
